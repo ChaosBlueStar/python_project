7b807398fd525fe4e86e9d1c602a34bfb53fb962
Jaehong Lee <118726854+ChaosBlueStar@users.noreply.github.com>
2022-12-20 15:10:47 +0900

    testing

--- test.py
+++ test.py
@@ -10,6 +10,16 @@ from openpyxl.worksheet.table import Table, TableStyleInfo
 import tkinter.ttk
 import tkinter as tk
 
+home = 'xl/test.xlsx'
+info_xl = 'xl/personal.xlsx'
+
+og_file = openpyxl.load_workbook(home, data_only=True)  # 초기 시트 위치 저장(값으로)
+info_file = openpyxl.load_workbook(info_xl, data_only=True)  # 개인정보, 빈소별 물품정보 저장 공간(값으)
+trade_xl='xl/trade.xlsx'
+trade_file=openpyxl.load_workbook(trade_xl,data_only=True)
+info_sheets = [info_file['빈소1']]  # 지금은 하나만 사용하지만 빈소 창이 생기면 9개로 늘어날 것임
+readsh=info_file['빈소1']  #Hong info_sheets에 리스트를 2개씩 사용해서 읽히지않아 추가 작성
+readtrade=trade_file['거래명세서'] #거래명세서 시트 읽어오기
 
 def exsave():  # 빈소에 등록 된 물품명 읽어오기
 
@@ -18,7 +28,7 @@ def exsave():  # 빈소에 등록 된 물품명 읽어오기
     for x in range(1, (readsh.max_row + 1)):  # 행의 끝까지 반복
         row.append(readsh.cell(x, 1).value)  # row에 값 넣기 / 1을 바꾸면 열이 바뀜
 
-    #print(*row)  # row 내의 목록 전체 출력 (테스트용)
+    print(*row)  # row 내의 목록 전체 출력 (테스트용)
 
 
 def exsave2():  # 거래명세서에 등록 된 물품명 읽어오기
@@ -31,23 +41,23 @@ def exsave2():  # 거래명세서에 등록 된 물품명 읽어오기
 
 def compare():
     a = {exsave() == exsave2()}
-    if a:
+    if a == "치즈김밥":
         print('일치')
     else:
         print('미일치')
 
 
+def write():
+    for x in range ((readtrade.max_row + 1), 6):
+        readtrade.cell(x).value = '2'
 
 
-home = 'xl/test.xlsx'
-info_xl = 'xl/personal.xlsx'
+write()
+trade_file.save('xl/trade.xlsx')
 
-og_file = openpyxl.load_workbook(home, data_only=True)  # 초기 시트 위치 저장(값으로)
-info_file = openpyxl.load_workbook(info_xl, data_only=True)  # 개인정보, 빈소별 물품정보 저장 공간(값으)
-trade_xl='xl/trade.xlsx'
-trade_file=openpyxl.load_workbook(trade_xl,data_only=True)
-info_sheets = [info_file['빈소1']]  # 지금은 하나만 사용하지만 빈소 창이 생기면 9개로 늘어날 것임
-readsh=info_file['빈소1']  #Hong info_sheets에 리스트를 2개씩 사용해서 읽히지않아 추가 작성
-readtrade=trade_file['거래명세서'] #거래명세서 시트 읽어오기
+
+
+
+
 
 compare()
